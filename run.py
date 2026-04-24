370cba967c9eae4bb2bc898a25c6c1e369226f93
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-04-25 02:43:27 +0300

    Configure socketio to use environment port

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
